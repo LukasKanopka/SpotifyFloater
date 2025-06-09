61e00d81678a4b78aa3b09edf0b83ea064201732
LukasKanopka <lukaskanopka@icloud.com>
2025-06-08 22:44:40 -0500

    Initial Commit

---
+++ SpotifyFloater/SpotifyFloaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyFloaterApp.swift
+//  SpotifyFloater
+//
+//  Created by Lukas Kanopka on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyFloaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotifyFloaterTests/SpotifyFloaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyFloaterTests.swift
+//  SpotifyFloaterTests
+//
+//  Created by Lukas Kanopka on 6/8/25.
+//
+
+import Testing
+@testable import SpotifyFloater
+
+struct SpotifyFloaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
